0eca4806d772de77d439883527223e3a290cb183
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-11-29 20:29:18 +0100

    2019 1

---
+++ 2019/1/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Advent
+import qualified Data.List.Split as S
+import qualified Data.Text as T
+import Control.Monad
+import Data.List
+import Lists
+
+testCases1 :: [(String, String)]
+testCases1 = []
+
+solution1 :: String -> String
+solution1 input = input
+
+testCases2 :: [(String, String)]
+testCases2 = []
+
+solution2 :: String -> String
+solution2 input = input
+
+main :: IO ()
+main = runAdvent 2019 1 solution1 testCases1
