ad26760da486a24253f0dc52c96e726608e13fed
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-11-29 20:50:38 +0100

    cleanup

--- 2019/1/Main.hs
+++ 2019/1/Main.hs
@@ -20,6 +20,5 @@ solution2 :: String -> String
 solution2 input = input
 
 main :: IO ()
--- main = runAdvent 2019 1 solution1 testCases1
-main = putStrLn "abcd"
+main = runAdvent 2019 1 solution1 testCases1
 
