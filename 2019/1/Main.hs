1d7264c6d55520997435363c2ecf92438f3aff40
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-01 06:54:22 +0100

    2019 1

--- 2019/1/Main.hs
+++ 2019/1/Main.hs
@@ -4,17 +4,34 @@ import Advent
 import Data.Text as T
 
 testCases1 :: [(String, String)]
-testCases1 = []
+testCases1 = [
+    ("12\n14\n1969", "658"),
+    ("12", "2"),
+    ("14", "2"),
+    ("1969", "654")
+    ]
 
 solution1 :: String -> String
-solution1 input = input
+solution1 input = show . sum $ fuel . read <$> Prelude.lines input
+
+fuel :: Int -> Int
+fuel mass = floor (fromIntegral mass / 3) - 2 
 
 testCases2 :: [(String, String)]
-testCases2 = []
+testCases2 = [
+    ("14", "2"),
+    ("1969", "966"),
+    ("100756", "50346")
+    ]
 
 solution2 :: String -> String
-solution2 input = input
+solution2 input = show . sum $ fuel' . read <$> Prelude.lines input
+
+fuel' :: Int -> Int
+fuel' mass
+  | mass < 9 = 0
+  | otherwise = let fm = fuel mass in fm + fuel' fm
 
 main :: IO ()
-main = runAdvent 2019 1 solution1 testCases1
+main = runAdvent 2019 1 solution2 testCases2
 
