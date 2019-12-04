67e8b2da1ca20d4ac5e25806f7b5292399fe619c
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-04 06:26:42 +0100

    2019 4

--- 2019/4/Main.hs
+++ 2019/4/Main.hs
@@ -9,23 +9,57 @@ import Control.Applicative
 
 testCases1 :: [(String, String)]
 testCases1 = [
-    ("", ""),
-    ("", "")
     ]
 
 solution1 :: String -> String
-solution1 input = input
+solution1 input = let
+    a = read $ take 6 input
+    z = read $ drop 7 input
+    in show $ length [(a1, a2, a3, a4, a5, a6) |
+        a1 <- [0..9],
+        a2 <- [0..9],
+        a3 <- [0..9],
+        a4 <- [0..9],
+        a5 <- [0..9],
+        a6 <- [0..9],
+        a2 >= a1,
+        a3 >= a2,
+        a4 >= a3,
+        a5 >= a4,
+        a6 >= a5,
+        let s = a1*100000 + a2*10000+a3*1000+a4*100+a5*10+a6,
+        s >= a,
+        s <= z,
+        a1 == a2 && a2 /= a3|| a2 == a3 && a1 /= a2 && a3 /= a4 || a3 == a4 && a2 /= a3 && a4 /= a5|| a4 == a5 && a3 /= a4 && a5 /= a6|| a5 == a6 && a4 /= a5 
+    ]
 
 testCases2 :: [(String, String)]
 testCases2 = [
-    ("", ""),
-    ("", "")
     ]
 
 solution2 :: String -> String
-solution2 input = input
+solution2 input = let
+    a = read $ take 6 input
+    z = read $ drop 7 input
+    in show $ length [(a1, a2, a3, a4, a5, a6) |
+        a1 <- [0..9],
+        a2 <- [0..9],
+        a3 <- [0..9],
+        a4 <- [0..9],
+        a5 <- [0..9],
+        a6 <- [0..9],
+        a2 >= a1,
+        a3 >= a2,
+        a4 >= a3,
+        a5 >= a4,
+        a6 >= a5,
+        let s = a1*100000 + a2*10000+a3*1000+a4*100+a5*10+a6,
+        s >= a,
+        s <= z,
+        a1 == a2|| a2 == a3 || a3 == a4 || a4 == a5 || a5 == a6
+    ]
 
 main :: IO ()
-main = runAdvent 2019 4 solution1 testCases1
--- main = runAdvent 2019 4 solution2 testCases2
+-- main = runAdvent 2019 4 solution1 testCases1
+main = runAdvent 2019 4 solution2 testCases2
 
