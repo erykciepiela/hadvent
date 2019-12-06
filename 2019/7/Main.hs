ad19fc0df85008bae9d70c95e85bd9aefe156b62
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-06 20:21:14 +0100

    2019 7

---
+++ 2019/7/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Advent
+import Data.Text as T
+import qualified Data.List as L
+import qualified Data.Set as S
+import Control.Monad
+import Control.Applicative
+import Data.Map as P
+import Data.Maybe
+
+solution1 :: String -> String
+solution1 input = input
+
+solution2 :: String -> String
+solution2 input = input
+
+main :: IO ()
+main = do
+    runAdvent 2019 7 solution1 []
+    runAdvent 2019 7 solution2 []
+
