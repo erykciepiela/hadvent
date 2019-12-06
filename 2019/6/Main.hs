d6be4d66dddcb63282b9e6f1a8c597fd1624a4ef
eryk.ciepiela <eryk.ciepiela@ocado.com>
2019-12-06 13:20:13 +0100

    2019 6

--- 2019/6/Main.hs
+++ 2019/6/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Advent
 import Data.Text as T
--- import Data.Text.Read as T
 import qualified Data.List as L
 import qualified Data.Set as S
 import Control.Monad
@@ -10,27 +9,33 @@ import Control.Applicative
 import Data.Map as P
 import Data.Maybe
 
-solution2 :: String -> String
-solution2 input = input
+solution2 :: Text -> Text -> String -> String
+solution2 a z input = let
+    pairs = parsePair (pack input)
+    m0 = P.fromList pairs
+    m = ffix closure m0
+    (Just start) = P.lookup a m
+    (Just stop) = P.lookup z m
+    in show $ snd $ L.head $ L.sortOn snd $ toList $ P.intersectionWith (+) start stop
 
 solution1 :: String -> String
 solution1 input = let
     pairs = parsePair (pack input)
-    in show $ sum $ S.size <$> ffix closure (P.fromAscList pairs)
+    in show $ sum $ P.size <$> ffix closure (P.fromList pairs)
 
-closure :: Ord a => P.Map a (S.Set a) -> P.Map a (S.Set a)
-closure m = (\sa -> L.foldr (\a s -> S.union s (fromMaybe S.empty (P.lookup a m))) sa sa) <$> m
+type Orb = Text
+
+parsePair :: Text -> [(Orb, P.Map Orb Int)]
+parsePair t = (\os -> let [a, b] = splitOn ")" os in (b, (P.singleton a 0))) <$> T.lines t
+
+closure :: Ord a => P.Map a (P.Map a Int) -> P.Map a (P.Map a Int)
+closure m = (\ma -> ma <> mconcat ((\(a, d) -> (+ (d + 1)) <$> fromMaybe mempty (P.lookup a m)) <$> P.toList ma)) <$> m
 
 ffix :: Eq a => (a -> a) -> a -> a
 ffix f a = if f a == a then a else ffix f (f a)
 
-type Orb = Text
-
-parsePair :: Text -> [(Orb, S.Set Orb)]
-parsePair t = (\os -> let [a, b] = splitOn ")" os in (b, S.singleton a)) <$> T.lines t
-
 main :: IO ()
 main = do
-    print $ ffix closure (P.fromAscList [(2, S.singleton 1), (3, S.singleton 2), (4, S.singleton 3), (5, S.singleton 4)])
     runAdvent 2019 6 solution1 []
+    runAdvent 2019 6 (solution2 "YOU" "SAN") []
 
