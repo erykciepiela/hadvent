bc61a4981ead3d7a39eddee1456320f8f1b3f451
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-07 17:07:18 +0100

    2019 8

---
+++ 2019/8/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Advent
+import Data.Text as T
+import Data.Text.Read as T
+import qualified Data.List as L
+import qualified Data.Set as S
+import Control.Monad
+import Control.Applicative
+import Data.Map as P
+import Data.Maybe
+import Control.Exception
+import Debug.Trace
+
+solution1 :: String -> String
+solution1 input = "?"
+
+solution2 :: String -> String
+solution2 input = "?"
+
+main :: IO ()
+main = advent 2019 8 [solution1, solution2] $ do
+    solution1 "" `shouldBe` "?"
+    solution1 "" `shouldBe` "?"
