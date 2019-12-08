553b94627b1d1f327a73f2994a600846e4502caf
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-08 09:09:03 +0100

    cleanup

--- 2019/8/Main.hs
+++ 2019/8/Main.hs
@@ -37,8 +37,15 @@ newtype SList a = SList { slist :: [a] }
 instance Semigroup a => Semigroup (SList a) where
     l1 <> l2 = SList $ L.foldl1 (<>) <$> L.transpose [slist l1, slist l2]
 
+composeLayers :: Int -> Int -> String -> String
+composeLayers w h input = L.unlines $ LS.chunksOf w $ mconcat $ fmap show $ slist $ L.foldl1 (<>) $ SList . fmap Color <$> LS.chunksOf (w * h) (parseDigits input)
+
 solution2 :: Int -> Int -> String -> String
-solution2 w h input = L.unlines $ LS.chunksOf w $ mconcat $ fmap show $ slist $ L.foldl1 (<>) $ SList . fmap Color <$> LS.chunksOf (w * h) (parseDigits input)
+solution2 w h input = render <$> composeLayers w h input
+    where
+        render '0' = 'B'
+        render '1' = ' '
+        render c = c
 
 main :: IO ()
 main = advent 2019 8 [solution1 25 6, solution2 25 6] $ do
@@ -46,11 +53,11 @@ main = advent 2019 8 [solution1 25 6, solution2 25 6] $ do
     parseDigits "123\n" `shouldBe` [1,2,3]
     LS.chunksOf 2 [1,2,3,4,5,6] `shouldBe` [[1,2], [3,4], [5,6]]
     solution1 3 2 "012121" `shouldBe` "6"
-    solution2 2 2 "0222112222120000" `shouldBe` "01\n10\n"
-    solution2 1 1 "01" `shouldBe` "0\n"
-    solution2 1 1 "11" `shouldBe` "1\n"
-    solution2 1 1 "21" `shouldBe` "1\n"
-    solution2 2 2 "22221111" `shouldBe` "11\n11\n"
-    solution2 2 2 "22221010" `shouldBe` "10\n10\n"
+    composeLayers 2 2 "0222112222120000" `shouldBe` "01\n10\n"
+    composeLayers 1 1 "01" `shouldBe` "0\n"
+    composeLayers 1 1 "11" `shouldBe` "1\n"
+    composeLayers 1 1 "21" `shouldBe` "1\n"
+    composeLayers 2 2 "22221111" `shouldBe` "11\n11\n"
+    composeLayers 2 2 "22221010" `shouldBe` "10\n10\n"
 
 -- answer two decoded: FKAHL
