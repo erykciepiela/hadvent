6bd0504c41c94051753540ec38e377bd26d6d27b
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-09 08:30:36 +0100

    2019 10

---
+++ 2019/10/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Advent
+import Utils
+import Data.Text as T
+import Data.Text.Read as T
+import qualified Data.List as L
+import qualified Data.Set as S
+import Control.Monad
+import Control.Applicative
+import Data.Map as P
+import Data.Maybe
+import Control.Exception
+import Debug.Trace
+import Data.List.Split as LS
+
+solution1 :: String -> String
+solution1 input = "?"
+
+solution2 :: String -> String
+solution2 input = "?"
+
+main :: IO ()
+main = advent 2019 10 [solution1, solution2] $ do
+    return ()
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ghcid --command "stack ghci hadvent:exe:2019-9 --ghci-options=-fobject-code" --test "main"
+ghcid --command "stack ghci hadvent:exe:2019-10 --ghci-options=-fobject-code" --test "main"
