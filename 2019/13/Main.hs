72bcfa7abead709c1e6036c72e47cd38f59a18bb
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-12 22:01:53 +0100

    2019 13

---
+++ 2019/13/Main.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Advent
+import Utils
+import Data.Text as T
+import Data.Text.Read as T
+import qualified Data.List as L
+import qualified Data.Set as S
+import Control.Monad
+import Control.Applicative
+import Data.Map as M
+import Data.Maybe
+import Control.Exception
+import Data.List.Split as LS
+import Data.Angle
+import Control.Comonad
+import qualified Data.List.Safe as LSF
+import Data.Semigroup
+import Data.Foldable as F
+import Debug.Trace
+import Data.Functor
+
+solution1 :: String -> String
+solution1 input = "?"
+        
+solution2 :: String -> String
+solution2 input = "?"
+
+main :: IO ()
+main = advent 2019 13 [solution1] $ do
+    return ()
