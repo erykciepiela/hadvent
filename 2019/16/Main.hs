598b8ba3501e29a15a9fae95957bc6831e4f888d
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-16 21:56:03 +0100

    2019 16

--- 2019/16/Main.hs
+++ 2019/16/Main.hs
@@ -42,7 +42,7 @@ solution1 input = L.take 8 $ sol1 input 100
 fft' :: [Int] -> Int -> [Int]
 fft' is 0 = is
 fft' is phn = let
-    rss = L.scanr1 (\a b -> (a+b)) is
+    rss = L.scanr1 (\a b -> a `seq` b `seq` (a+b)) is
     in fft rss (phn - 1)
 
 solution2 :: String -> String
@@ -56,5 +56,5 @@ main = advent 2019 16 [solution1, solution2] $ do
     sol1 "80871224585914546619083218645595" 100 `shouldBe` "24176176"
     sol1 "19617804207202209144916044189917" 100 `shouldBe` "73745418"
     sol1 "69317163492948606335995924319873" 100 `shouldBe` "52432133"
-    (L.scanr1 (+) [4,3,2,1]) `shouldBe` [10,6,3,1]
+    L.scanr1 (+) [4,3,2,1] `shouldBe` [10,6,3,1]
     return ()
