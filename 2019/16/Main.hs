e9bc1ad4ee3cc7381da0b0461cdd628997f9af15
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-16 08:40:41 +0100

    2019 16

--- 2019/16/Main.hs
+++ 2019/16/Main.hs
@@ -24,12 +24,27 @@ import Text.Parsec
 import Text.Parsec.Char 
 import Text.Parsec.Combinator as P
 
+fft :: [Int] -> Int -> [Int]
+fft is phn = let
+    phs = (\n -> L.take (L.length is) $ L.tail $ L.cycle (L.replicate n 0 <> L.replicate n 1 <> L.replicate n 0 <> L.replicate n (-1))) <$> [1..(L.length is)]
+    rs = L.iterate (\is -> (\p -> (`mod` 10) $ abs $ L.sum $ L.zipWith (*) is p) <$> phs) is 
+    in (L.take 8 $ rs !! phn)
+
+sol1 :: String -> Int -> String
+sol1 input phn = let
+    ints = read . (\c -> [c]) <$> input
+    in mconcat $ show <$> fft ints phn
+
 solution1 :: String -> String
-solution1 input = "?"
+solution1 input = L.take 8 $ sol1 input 100
 
 solution2 :: String -> String
-solution2 input = "?"
+solution2 input = L.take 8 $ sol1 (mconcat (L.replicate 10000 input)) 100
     
 main :: IO ()
 main = advent 2019 16 [solution1, solution2] $ do
+    sol1 "12345678" 4 `shouldBe` "01029498"
+    sol1 "80871224585914546619083218645595" 100 `shouldBe` "24176176"
+    sol1 "19617804207202209144916044189917" 100 `shouldBe` "73745418"
+    sol1 "69317163492948606335995924319873" 100 `shouldBe` "52432133"
     return ()
