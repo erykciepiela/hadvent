487fb2904cc8897938f3f7d722e80f5fde7778c3
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-18 00:15:01 +0100

    2019 18

---
+++ 2019/18/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Advent
+import Utils
+import Data.Text as T
+import Data.Text.Read as T
+import qualified Data.List as L
+import qualified Data.Set as S
+import Control.Monad
+import Control.Applicative
+import Data.Map as M
+import Data.Maybe
+import Control.Exception
+import Data.List.Split as LS
+import Data.Angle
+import Control.Comonad
+import qualified Data.List.Safe as LSF
+import Data.Semigroup
+import Data.Foldable as F
+import Debug.Trace
+import Data.Functor
+import Data.ByteString.Char8 as C
+import Data.Char
+-- import Text.Parsec
+-- import Text.Parsec.Char 
+-- import Text.Parsec.Combinator as P
+
+solution1 :: String -> String
+solution1 input = "?"
+
+
+solution2 :: String -> String
+solution2 input = "?"
+    
+main :: IO ()
+main = advent 2019 18 [solution1] $ do
+    return ()
