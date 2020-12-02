58d46034a47b05132d155c2244485ee36f3da9d0
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-12-02 17:03:32 +0100

    2020-1

--- 2020/1/Main.hs
+++ 2020/1/Main.hs
@@ -4,13 +4,13 @@ import Advent
 
 solution1 :: String -> String
 solution1 input = let
-  (items :: [Int]) = read <$> lines input
-  in head [show (a * b) | a <- items, b <-items, a + b == 2020]
+  items = read <$> lines input
+  in (show . head) [a * b | a <- items, b <-items, a + b == 2020]
 
 solution2 :: String -> String
 solution2 input = let
-  (items :: [Int]) = read <$> lines input
-  in head [show (a * b * c) | a <- items, b <-items, c <- items, a + b + c == 2020]
+  items = read <$> lines input
+  in (show . head) [a * b * c | a <- items, b <-items, c <- items, a + b + c == 2020]
 
 main :: IO ()
 main = advent 2020 1 [solution2] $ do
