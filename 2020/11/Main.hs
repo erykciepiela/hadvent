a5eb525a2735246be6a4e0a509c9c3de09126ae4
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-12-10 21:48:29 +0100

    2020-11

---
+++ 2020/11/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Advent
+
+import Text.Parsec as P
+import Text.Parsec.String
+import Data.Functor
+import Data.List as L
+import Control.Monad
+import Data.Set as S
+-- import Data.Map as M
+import Data.Maybe
+
+solution1 :: String -> Int
+solution1 input = 1
+
+
+solution2 :: String -> Int
+solution2 input = 1
+
+main :: IO ()
+main = advent 2020 11 [solution1] $ do
+  return ()
