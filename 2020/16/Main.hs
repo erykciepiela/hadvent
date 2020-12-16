3c4e419445c0b3d8d2d69505a29ad2750bc32031
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-12-16 10:51:50 +0100

    2020-16

--- 2020/16/Main.hs
+++ 2020/16/Main.hs
@@ -18,20 +18,59 @@ import Data.Map as M
 
 import Debug.Trace
 
-baz :: Int -> (Int, Int, Map Int Int) -> (Int, Int, Map Int Int)
-baz limit (l, s, prevs) = if s == limit then (l, s, prevs) else case M.lookup l prevs of
-  Nothing -> baz limit (0, s + 1, M.insert l (s - 1) prevs)
-  Just i -> baz limit (s - i - 1, s + 1, M.insert l (s - 1) prevs)
 
-solution1 :: String -> Int
+data Input = Input {
+  rules :: [Rule],
+  myTicket :: Ticket,
+  nearbyTickets :: [Ticket]
+} deriving Show
+
+data Rule = Rule {
+  fieldName :: String,
+  ranges :: [(Int, Int)]
+} deriving Show
+
+ruleParser :: Parser Rule
+ruleParser = do
+  fieldName <- many1 $ noneOf "\n:"
+  string ": "
+  ranges <- (do
+    from <- read <$> many1 digit
+    char '-'
+    to <- read <$> many1 digit
+    return (from, to)) `sepBy` string " or "
+  return $ Rule fieldName ranges
+
+data Ticket = Ticket {
+  ticketValues :: [Int]
+ } deriving Show
+
+ticketParser :: Parser Ticket
+ticketParser = Ticket <$> (read <$> many1 digit) `sepBy` char ','
+
+inputParser :: Parser Input
+inputParser = do
+  rules <- ruleParser `sepEndBy` char '\n'
+  string "\nyour ticket:\n"
+  myTicket <- ticketParser
+  string "\n\nnearby tickets:\n"
+  nearbyTickets <- ticketParser `sepBy` char '\n'
+  return $ Input rules myTicket nearbyTickets
+
+
+solution1 :: String -> String
 solution1 input = let
-  ns :: [Int] = either (error "wrong parser") id $ parse ((read <$> many1 digit) `sepBy` char ',') "?" input
-  in (\(l, _, _) -> l) $ baz 2020 (last ns, length ns, M.fromList $ zip ns [0..(length ns) - 2])
+  input' :: Input = either (error "wrong parser") id $ parse inputParser "?" input
+  nearbyTicketsValues = mconcat (ticketValues <$> nearbyTickets input')
+  allRulesRanges = mconcat $ ranges <$> rules input'
+  -- in show nearbyTicketsValues <> "\n" <> show allRulesRanges
+  in show $ sum $ L.filter (outsideRanges allRulesRanges) nearbyTicketsValues
+  -- in show $ L.filter (outsideRanges allRulesRanges) nearbyTicketsValues
 
-solution2 :: String -> Int
-solution2 input = let
-  ns :: [Int] = either (error "wrong parser") id $ parse ((read <$> many1 digit) `sepBy` char ',') "?" input
-  in (\(l, _, _) -> l) $ baz 30000000 (last ns, length ns, M.fromList $ zip ns [0..(length ns) - 2])
+outsideRanges :: [(Int, Int)] -> Int -> Bool
+outsideRanges [] _ = True
+outsideRanges ((from, to):ranges) n = if (n >= from && n <= to) then False else outsideRanges ranges n
+
 
 main :: IO ()
 main = advent 2020 16 [solution1] $ do
