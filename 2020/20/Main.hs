a68cbb75689fcc75673b9a4285a8cad17b88eb1a
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-12-20 22:36:09 +0100

    2020-20

--- 2020/20/Main.hs
+++ 2020/20/Main.hs
@@ -30,27 +30,26 @@ data Tile = Tile {
 
 inputParser :: Parser [Tile]
 inputParser = tile `sepEndBy` newline
-
-tile :: Parser Tile
-tile = do
-  tileId <- between (string "Tile ") (string ":\n") (read <$> many1 digit)
-  border <- count 10 (count 10 symbol <* newline)
-  return $ Tile tileId [head border, last <$> border, last border, head <$> border]
-
-symbol :: Parser Bool
-symbol = (False <$ char '.') <|> (True <$ char '#')
+  where
+    tile :: Parser Tile
+    tile = do
+      tileId <- between (string "Tile ") (string ":\n") (read <$> many1 digit)
+      border <- count 10 (count 10 symbol <* newline)
+      return $ Tile tileId [head border, last <$> border, last border, head <$> border]
+        where
+          symbol :: Parser Bool
+          symbol = (False <$ char '.') <|> (True <$ char '#')
 
 solution1 :: String -> Int
 solution1 input = let
   tiles = either (error "wrong input parser") id $ parse inputParser "" input
-  cornerTiles = L.filter (isOnCorner tiles) tiles
-  in product $ tileId <$> cornerTiles
-
-isOnCorner :: [Tile] -> Tile -> Bool
-isOnCorner tiles tile = let
-  ts = L.filter (\tile' -> tileId tile' /= tileId tile) tiles
-  bs = tileBorder tile <&> (\side -> any (any (\side' -> side' == side || reverse side' == side) . tileBorder) ts)
-  in length (L.filter (== False) bs) == 2
+  in product $ tileId <$> L.filter (isOnCorner tiles) tiles
+    where
+      isOnCorner :: [Tile] -> Tile -> Bool
+      isOnCorner tiles tile = let
+        ts = L.filter (\tile' -> tileId tile' /= tileId tile) tiles
+        bs = tileBorder tile <&> (\side -> any (any (\side' -> side' == side || reverse side' == side) . tileBorder) ts)
+        in length (L.filter (== False) bs) == 2
 
 main :: IO ()
 main = advent 2020 20 [solution1] $ do
