cbd90b2db07cb4f1e81f464e93740594bbb3fd5f
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-12-23 10:19:43 +0100

    2020-23

--- 2020/23/Main.hs
+++ 2020/23/Main.hs
@@ -23,54 +23,44 @@ import Debug.Trace
 
 import Data.Either
 
-type Deck = [Int]
+import Data.Char
 
-inputParser :: Parser (Deck, Deck)
+newtype Ring = Ring [Int] deriving Show
+
+turnRing :: Ring -> Ring
+turnRing (Ring nums) = let
+  cursor = head nums
+  threesome = L.take 3 $ tail nums
+  possibleDestinations = zip [5..] (L.drop 4 nums)
+  (destinationIndex, destinationVal) = head $ sortOn ((`mod` length nums). (-) cursor. snd) possibleDestinations
+  newNums = L.drop 4 (L.take destinationIndex nums) <> threesome <> L.drop destinationIndex nums <> [cursor]
+  -- in trace (show newNums <> " " <> show destinationIndex <> " " <> show destinationVal) (Ring newNums)
+  in (Ring newNums)
+
+labels :: Ring -> String
+labels (Ring nums) = let
+  (post, pre) = L.break (== 1) nums
+  in intToDigit <$> tail (pre <> post)
+
+inputParser :: Parser Ring
 inputParser = do
-  [d1, d2] <- deck `sepEndBy` newline
-  return (d1, d2)
-  where
-    deck :: Parser Deck
-    deck = do
-      between (string "Player ") (string ":\n") num
-      num `sepEndBy` newline
-        where
-          num :: Parser Int
-          num = read <$> many1 digit
+  nums <- fmap digitToInt <$> many1 digit
+  return $ Ring nums
 
-solution1 :: String -> Int
+inputParser2 :: Parser Ring
+inputParser2 = do
+  nums <- fmap digitToInt <$> many1 digit
+  return $ Ring $ nums <> [10..1000000]
+
+solution1 :: String -> String
 solution1 input = let
-  (deck1, deck2) = either (\e -> error $ "wrong input parser: " <> show e) id $ parse inputParser "" input
-  winningDeck = case playRound (deck1, deck2) of
-    ([], d2) -> d2
-    (d1, _) -> d1
-  in sum $ zipWith (*) [1..] (reverse winningDeck)
-    where
-      playRound :: (Deck, Deck) -> (Deck, Deck)
-      playRound ds@(d, []) = ds
-      playRound ds@([], d) = ds
-      playRound ds@(c1:d1, c2:d2) = if c1 > c2
-          then playRound (d1 <> [c1, c2], d2)
-          else playRound (d1, d2 <> [c2, c1])
+  ring = either (\e -> error $ "wrong input parser: " <> show e) id $ parse inputParser "" input
+  in Main.labels $ iterate turnRing ring !! 100
 
-solution2 :: String -> Int
+solution2 :: String -> String
 solution2 input = let
-  (deck1, deck2) = either (\e -> error $ "wrong input parser: " <> show e) id $ parse inputParser "" input
-  winningDeck = case playRound [] (deck1, deck2) of
-    ([], d2) -> d2
-    (d1, _) -> d1
-  in sum $ zipWith (*) [1..] (reverse winningDeck)
-    where
-      playRound :: [(Deck, Deck)] -> (Deck, Deck) -> (Deck, Deck)
-      playRound _ ds@(d, []) = ds
-      playRound _ ds@([], d) = ds
-      playRound history ds = if ds `elem` history then ds else case ds of
-        (c1:d1, c2:d2) | c1 <= length d1 && c2 <= length d2 -> case playRound [] (L.take c1 d1, L.take c2 d2) of
-          ([], _) -> playRound (ds:history) (d1, d2 <> [c2, c1])
-          (_, _) -> playRound (ds:history) (d1 <> [c1, c2], d2)
-        ds@(c1:d1, c2:d2) -> if c1 > c2
-          then playRound (ds:history) (d1 <> [c1, c2], d2)
-          else playRound (ds:history) (d1, d2 <> [c2, c1])
+  ring = (either (\e -> error $ "wrong input parser: " <> show e) id $ parse inputParser2 "" input)
+  in Main.labels $ iterate turnRing ring !! 10000000
 
 main :: IO ()
 main = advent 2020 23 [solution2] $ do
