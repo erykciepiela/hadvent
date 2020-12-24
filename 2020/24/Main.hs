457f01996b7d077c09cb90ff9c782f0438efac66
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2020-12-24 13:09:32 +0100

    2020-24

--- 2020/24/Main.hs
+++ 2020/24/Main.hs
@@ -32,7 +32,8 @@ data Tile = Tile {
   n :: Int
 } deriving (Show, Eq, Ord)
 
-neighTiles (Tile e n) = [
+adjacentTiles :: Tile -> [Tile]
+adjacentTiles (Tile e n) = [
   Tile (e-1) n,
   Tile (e+1) n,
   Tile (e + if even (n+1) then 1 else 0) (n+1),
@@ -43,48 +44,45 @@ neighTiles (Tile e n) = [
 
 inputParser :: Parser [Tile]
 inputParser = tileParser `sepEndBy` newline
+  where
+    tileParser :: Parser Tile
+    tileParser = do
+      dirs <- many1 $ P.choice
+        [ SE <$ try (string "se")
+        , SW <$ try (string "sw")
+        , E <$ try (string "e")
+        , NE <$ try (string "ne")
+        , NW <$ try (string "nw")
+        , W <$ string "w"
+        ]
+      let se = length (L.filter (== SE) dirs) - length (L.filter (== NW) dirs)
+      let ne = length (L.filter (== NE) dirs) - length (L.filter (== SW) dirs)
+      return $ Tile
+        (length (L.filter (== E) dirs) - length (L.filter (== W) dirs) + (ne + se) `div` 2)
+        (ne - se)
 
-tileParser :: Parser Tile
-tileParser = do
-  dirs <- many1 $
-    (SE <$ try (string "se")) <|>
-    (SW <$ try (string "sw")) <|>
-    (E <$ try (string "e")) <|>
-    (NE <$ try (string "ne")) <|>
-    (NW <$ try (string "nw")) <|>
-    (W <$ string "w")
-  let se = length (L.filter (== SE) dirs) - length (L.filter (== NW) dirs)
-  let ne = length (L.filter (== NE) dirs) - length (L.filter (== SW) dirs)
-  return $ Tile
-    (length (L.filter (== E) dirs) - length (L.filter (== W) dirs) + (ne + se) `div` 2)
-    (ne - se)
-
-foo :: Tile -> Tile -> Tile
-foo t1 t2 = Tile (e t1 + e t2 + (n t1 + n t2) `div` 2) (n t1 - n t2)
-
-solution1 :: String -> String
+solution1 :: String -> Int
 solution1 input = let
   tiles = either (\e -> error $ "wrong input parser: " <> show e) id $ parse inputParser "" input
   a = M.unionsWith (+) ((`M.singleton` 1) <$> tiles)
-  in show $ length $ L.filter id $ odd <$> M.elems a
+  in length $ L.filter id $ odd <$> M.elems a
 
-solution2 :: String -> String
+solution2 :: String -> Int
 solution2 input = let
   tiles = either (\e -> error $ "wrong input parser: " <> show e) id $ parse inputParser "" input
   a = M.unionsWith (+) ((`M.singleton` 1) <$> tiles)
-  back = M.fromList [(Tile e n, 0) | e <- [-121, -120 .. 121], n <- [-121, -120 .. 121]]
-  b = M.unionWith (+) back a
-  in show $ length $ L.filter odd $ M.elems $ iterate foo b !! 100
-  -- in show $ a
+  background = M.fromList [(Tile e n, 0) | e <- [-121, -120 .. 121], n <- [-121, -120 .. 121]]
+  b = M.unionWith (+) background a
+  in length $ L.filter odd $ M.elems $ iterate iteration b !! 100
     where
-      foo :: M.Map Tile Int -> M.Map Tile Int
-      foo m = M.mapWithKey (\tile v -> let
-        blackNeighs = L.length (L.filter odd (catMaybes ((`M.lookup` m) <$> neighTiles tile)))
+      iteration :: M.Map Tile Int -> M.Map Tile Int
+      iteration m = M.mapWithKey (\tile v -> let
+        blackNeighs = L.length (L.filter odd (catMaybes ((`M.lookup` m) <$> adjacentTiles tile)))
         in case (v, blackNeighs) of
           (v, blackNeighs) | odd v && (blackNeighs == 0 || blackNeighs > 2) -> 0
           (v, blackNeighs) | even v && blackNeighs == 2 -> 1
           (v, _) -> v) m
 
 main :: IO ()
-main = advent 2020 24 [solution2] $ do
+main = advent 2020 24 [solution1] $ do
   return ()
